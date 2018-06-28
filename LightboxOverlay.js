4f77fafeb3c21a1828bc0294aa292eac38f4199c
oBlank <dyh1919@gmail.cm>
2018-06-28 15:19:30 +0800

    update

--- LightboxOverlay.js
+++ LightboxOverlay.js
@@ -486,7 +486,10 @@ export default class LightboxOverlay extends Component {
       height: openVal.interpolate({ inputRange: [ 0, 1 ], outputRange: [ origin.height, WINDOW_HEIGHT ] }),
     } ];
 
-    const hideIconsStyle = { display: (this.hideIcons || this.state.scale > 1) ? 'none' : 'flex' }
+    const hideIconsStyle = null//{ display: (this.hideIcons || this.state.scale > 1) ? 'none' : 'flex' }
+    if (this.hideIcons || this.state.scale > 1) {
+      lightboxOpacityStyle.opacity = new Animated.Value(0)
+    }
     this.preScale = this.state.scale;
 
 
